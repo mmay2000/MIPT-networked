7fbf713ad314c303bb717944c29c84ae2f98002e
mmay2000 <102404342+mmay2000@users.noreply.github.com>
2023-02-17 19:37:56 +0300

    hw2

--- w2/client.cpp
+++ w2/client.cpp
@@ -1,5 +1,7 @@
 #include <enet/enet.h>
 #include <iostream>
+#include <thread>
+#include <string>
 
 void send_fragmented_packet(ENetPeer *peer)
 {
@@ -25,6 +27,24 @@ void send_micro_packet(ENetPeer *peer)
   enet_peer_send(peer, 1, packet);
 }
 
+
+void InputStart(ENetPeer* serv, ENetPeer* lob)
+{
+    while (!serv) {
+        std::string input;
+        std::getline(std::cin, input);
+        if (strcmp(input.data(), "start") == 0)
+        {
+            printf("Starting the game...\n");
+            const char* msg = "start";
+            ENetPacket* packet = enet_packet_create(msg, strlen(msg) + 1, ENET_PACKET_FLAG_RELIABLE);
+            enet_peer_send(lob, 0, packet);
+            break;
+        }
+    }
+}
+
+
 int main(int argc, const char **argv)
 {
   if (enet_initialize() != 0)
@@ -33,28 +53,37 @@ int main(int argc, const char **argv)
     return 1;
   }
 
-  ENetHost *client = enet_host_create(nullptr, 1, 2, 0, 0);
+  ENetHost *client = enet_host_create(nullptr, 2, 2, 0, 0);
   if (!client)
   {
     printf("Cannot create ENet client\n");
     return 1;
   }
 
-  ENetAddress address;
-  enet_address_set_host(&address, "localhost");
-  address.port = 10887;
+  
 
-  ENetPeer *lobbyPeer = enet_host_connect(client, &address, 2, 0);
+  ENetAddress address_lobby;
+  enet_address_set_host(&address_lobby, "localhost");
+  address_lobby.port = 10887;
+
+  ENetPeer* serverPeer = nullptr;
+  ENetPeer* lobbyPeer = enet_host_connect(client, &address_lobby, 2, 0);
   if (!lobbyPeer)
   {
     printf("Cannot connect to lobby");
     return 1;
   }
 
+  std::thread inputThread(InputStart, serverPeer, lobbyPeer);
+  inputThread.detach();
+
+
   uint32_t timeStart = enet_time_get();
   uint32_t lastFragmentedSendTime = timeStart;
   uint32_t lastMicroSendTime = timeStart;
   bool connected = false;
+  bool inGame = false;
+  
   while (true)
   {
     ENetEvent event;
@@ -62,32 +91,43 @@ int main(int argc, const char **argv)
     {
       switch (event.type)
       {
-      case ENET_EVENT_TYPE_CONNECT:
-        printf("Connection with %x:%u established\n", event.peer->address.host, event.peer->address.port);
-        connected = true;
-        break;
-      case ENET_EVENT_TYPE_RECEIVE:
-        printf("Packet received '%s'\n", event.packet->data);
-        enet_packet_destroy(event.packet);
-        break;
-      default:
-        break;
+          case ENET_EVENT_TYPE_CONNECT:
+            printf("Connection with %x:%u established\n", event.peer->address.host, event.peer->address.port);
+            connected = true;
+            break;
+          case ENET_EVENT_TYPE_RECEIVE:
+            printf("Packet received '%s'\n", event.packet->data);
+            if (event.packet->data[0] == '!')
+            {
+                
+                ENetAddress address_server;
+                enet_address_set_host(&address_server, "localhost");
+                address_server.port = std::atoi((const char*)(event.packet->data + 1));
+
+                serverPeer = enet_host_connect(client, &address_server, 2, 0);
+                if (!serverPeer)
+                {
+                    printf("Cannot connect to server");
+                    return 1;
+                }
+                inGame = true;
+            }
+            enet_packet_destroy(event.packet);
+            break;
+          default:
+            break;
       };
     }
-    if (connected)
+    if (inGame && serverPeer != nullptr)
     {
       uint32_t curTime = enet_time_get();
-      if (curTime - lastFragmentedSendTime > 1000)
-      {
-        lastFragmentedSendTime = curTime;
-        send_fragmented_packet(lobbyPeer);
-      }
-      if (curTime - lastMicroSendTime > 100)
-      {
-        lastMicroSendTime = curTime;
-        send_micro_packet(lobbyPeer);
-      }
+      srand(time(0));
+      std::string msg = "My time = " + std::to_string(curTime + rand());
+      ENetPacket* packet = enet_packet_create(msg.c_str(), strlen(msg.c_str()) + 1, ENET_PACKET_FLAG_RELIABLE);
+      enet_peer_send(serverPeer, 0, packet);
     }
   }
+
+  inputThread.join();
   return 0;
 }
--- w2/lobby.cpp
+++ w2/lobby.cpp
@@ -1,8 +1,13 @@
 #include <enet/enet.h>
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main(int argc, const char **argv)
 {
+
+  std::vector<ENetPeer*> connectedClients;
+
   if (enet_initialize() != 0)
   {
     printf("Cannot init ENet");
@@ -13,35 +18,70 @@ int main(int argc, const char **argv)
   address.host = ENET_HOST_ANY;
   address.port = 10887;
 
-  ENetHost *server = enet_host_create(&address, 32, 2, 0, 0);
+  ENetHost *lobby = enet_host_create(&address, 32, 2, 0, 0);
 
-  if (!server)
+  if (!lobby)
   {
     printf("Cannot create ENet server\n");
     return 1;
   }
 
+  /*
+  ENetAddress address_server;
+  enet_address_set_host(&address_server, "localhost");
+  address_server.port = 10888;
+
+  ENetPeer* serverPeer = enet_host_connect(lobby, &address_server, 2, 0);
+  if (!serverPeer)
+  {
+      printf("Cannot connect to server");
+      return 1;
+  }
+  */
+  bool sessionStarted = false;
+
   while (true)
   {
     ENetEvent event;
-    while (enet_host_service(server, &event, 10) > 0)
+    while (enet_host_service(lobby, &event, 10) > 0)
     {
       switch (event.type)
       {
-      case ENET_EVENT_TYPE_CONNECT:
-        printf("Connection with %x:%u established\n", event.peer->address.host, event.peer->address.port);
-        break;
-      case ENET_EVENT_TYPE_RECEIVE:
-        printf("Packet received '%s'\n", event.packet->data);
-        enet_packet_destroy(event.packet);
-        break;
-      default:
-        break;
+          case ENET_EVENT_TYPE_CONNECT:
+            printf("Connection with %x:%u established\n", event.peer->address.host, event.peer->address.port);
+           
+                connectedClients.push_back(event.peer);
+
+                if (sessionStarted)
+                {
+                    char server_data[] = "!10888";
+                    ENetPacket* packet = enet_packet_create(server_data, strlen(server_data) + 1, ENET_PACKET_FLAG_RELIABLE);
+                    enet_peer_send(event.peer, 0, packet);
+                }
+           
+
+            break;
+          case ENET_EVENT_TYPE_RECEIVE:
+            printf("Packet received '%s'\n", event.packet->data);
+            if (std::string((char*)(event.packet->data)) == "start")
+            {
+                char server_data[] = "!10888";
+                for (ENetPeer* p : connectedClients)
+                {
+                    ENetPacket* packet = enet_packet_create(server_data, strlen(server_data)+1, ENET_PACKET_FLAG_RELIABLE);
+                    enet_peer_send(p, 0, packet);
+                }
+                sessionStarted = true;
+            }
+            enet_packet_destroy(event.packet);
+            break;
+          default:
+            break;
       };
     }
   }
 
-  enet_host_destroy(server);
+  enet_host_destroy(lobby);
 
   atexit(enet_deinitialize);
   return 0;
